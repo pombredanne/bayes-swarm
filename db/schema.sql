e7060eea4331acc4862615161f72f96d71c325d9
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2007-07-30 21:03:03 +0000

    * db/schema.sql: added int_words table in order to list words that we want to monitor * added prototype1/python: webpy interface for db access, allows to add words to the interesting list or to show words/int_words * prototype1/ruby/swarm_support.rb: changed accordingly (needs to be cleaned up a bit)

--- db/schema.sql
+++ db/schema.sql
@@ -1,57 +1,82 @@
-drop table if exists associations ;
+drop table if exists associations;
+drop table if exists int_words;
 drop table if exists words;
 drop table if exists pages;
 drop table if exists sources;
 
 CREATE
-	TABLE sources
-	(
-		id int(11) NOT NULL AUTO_INCREMENT,
-		name varchar(100) NOT NULL,
-		PRIMARY KEY USING BTREE(id)
-	)
-	ENGINE=InnoDB; 
+    TABLE sources
+    (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        name varchar(100) NOT NULL,
+        PRIMARY KEY USING BTREE(id)
+    )
+    ENGINE=InnoDB;
 
 CREATE
-	TABLE pages
-	(
-		id int(11) NOT NULL AUTO_INCREMENT,
-		source_id int(11) NOT NULL,
-		url varchar(255) NOT NULL,
-		PRIMARY KEY USING BTREE(id),
-		constraint fk_page_source foreign key(source_id) references sources(id)
-	)
-	ENGINE=InnoDB;
-	
-CREATE 
-	TABLE words
-	(
-		id int(11) NOT NULL AUTO_INCREMENT,
-		page_id int(11) NOT NULL,
-		scantime DATETIME NOT NULL,
-		name varchar(255) NOT NULL,
-		count int(11) NOT NULL DEFAULT 0,
-		titlecount int(11) NOT NULL DEFAULT 0,
-		weight decimal(6,3) NOT NULL DEFAULT 0.0,
-		PRIMARY KEY USING BTREE(id),
-		constraint fk_word_page foreign key(page_id) references pages(id)
-	)
-	ENGINE=InnoDB;
-
+    TABLE pages
+    (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        source_id int(11) NOT NULL,
+        url varchar(255) NOT NULL,
+        PRIMARY KEY USING BTREE(id),
+        constraint fk_page_source foreign key(source_id) references sources(id)
+    )
+    ENGINE=InnoDB;
 
 CREATE
-	TABLE associations
-	(
-		words_from_id int(11) NOT NULL,
-		words_to_id int(11) NOT NULL,
-		cdist1 int(11) NOT NULL DEFAULT 0,
-		cdist2 int(11) NOT NULL DEFAULT 0,
-		cdist3 int(11) NOT NULL DEFAULT 0,
-		cdist4 int(11) NOT NULL DEFAULT 0,
-		cdist5 int(11) NOT NULL DEFAULT 0,
-		weight decimal(6,3) NOT NULL DEFAULT 0.0,
-		PRIMARY KEY USING BTREE(words_from_id, words_to_id),
-		constraint fk_assoc_from_word foreign key(words_from_id) references words(id),
-		constraint fK_assoc_to_word foreign key(words_to_id) references words(id)
-	)
-	ENGINE=InnoDB;
+    TABLE int_words
+    (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        name varchar(255) NOT NULL,
+        PRIMARY KEY USING BTREE(id)
+    )
+    ENGINE=InnoDB;
+
+CREATE
+    TABLE words
+    (
+        id int(11) NOT NULL,
+        page_id int(11) NOT NULL,
+        scantime DATETIME NOT NULL,
+        count int(11) NOT NULL DEFAULT 0,
+        titlecount int(11) NOT NULL DEFAULT 0,
+        weight decimal(6,3) NOT NULL DEFAULT 0.0,
+        PRIMARY KEY USING BTREE(id, page_id, scantime),
+        constraint fk_word_intword foreign key(id) references int_words(id),
+        constraint fk_word_page foreign key(page_id) references pages(id)
+    )
+    ENGINE=InnoDB;
+
+CREATE
+    TABLE associations
+    (
+        words_from_id int(11) NOT NULL,
+        words_to_id int(11) NOT NULL,
+        cdist1 int(11) NOT NULL DEFAULT 0,
+        cdist2 int(11) NOT NULL DEFAULT 0,
+        cdist3 int(11) NOT NULL DEFAULT 0,
+        cdist4 int(11) NOT NULL DEFAULT 0,
+        cdist5 int(11) NOT NULL DEFAULT 0,
+        weight decimal(6,3) NOT NULL DEFAULT 0.0,
+        PRIMARY KEY USING BTREE(words_from_id, words_to_id),
+        constraint fk_assoc_from_word foreign key(words_from_id) references words(id),
+        constraint fK_assoc_to_word foreign key(words_to_id) references words(id)
+    )
+    ENGINE=InnoDB;
+
+INSERT INTO
+    int_words
+        (name)
+    values
+        ('china'),
+        ('india'),
+        ('iraq'),
+        ('terror'),
+        ('muslim'),
+        ('islam'),
+        ('bomb'),
+        ('bush'),
+        ('you-tube'),
+        ('italy'),
+        ('porn')
--- prototype1/ruby/swarm_support.rb
+++ prototype1/ruby/swarm_support.rb
@@ -9,7 +9,18 @@ class StemCount
   end
 end
 
-def count_stems(stems)
+class StemCountId
+  attr_accessor :stem , :count, :id
+
+  def initialize(stem, count, id)
+    @id = id
+    @stem = stem
+    @count = count
+  end
+end
+
+
+def count_stems(stems, int_stems)
   stem_count = Hash.new
   stems.each do |stem|
     stem_count[stem] ||= 0
@@ -21,34 +32,48 @@ def count_stems(stems)
     res << StemCount.new(s,c) unless s.length <= 2 || s =~ /\d+/
   end
 
-  res = res.sort_by { |sc| sc.count }.reverse
-  res
-end
-
-def match_interesting_stems(stems, interesting_stems)
-  res = Array.new
-  stems.each do |stem|
-    interesting_stems.each do |int_stem|
-      if stem.stem == int_stem
-        res << stem
+  # check if it is among interesting stems
+  # FIXME: if int_stems is not passed, count_stems should return the
+  # full list of stems without filtering
+  res2 = Array.new
+  res.each do |stem|
+    int_stems.each do |int_stem|
+      if stem.stem == int_stem.stem
+        res2 << StemCountId.new(stem.stem, stem.count, int_stem.id)
       end
     end
   end
-  res
+
+  res2 = res2.sort_by { |sc| sc.count }.reverse
+  res2
 end
 
-def insert_into_db(stems)
-  dbh = Mysql.real_connect("localhost", "matteo", "test", "bayesfortest")
-  dbh.query("DROP TABLE IF EXISTS words")
-  dbh.query("CREATE TABLE words (
-                                 stem char(100) NOT NULL,
-                                 count int(11),
-                                 scantime DATETIME NOT NULL
-                                 ) ")
+def get_interesting_stems()
+  dbh = Mysql.real_connect("localhost", "testuser", "test", "bayesfortest")
+  res = dbh.query("SELECT id, name FROM int_words")
 
-  res = dbh.prepare "INSERT INTO words (stem, count, scantime) VALUES (?, ?, ?)"
+  puts "Interesting stems are:"
+  res2 = Array.new
+  while row = res.fetch_row do
+    id = row[0]
+    name = row[1]
+    printf "%s, %s\n", id, name
+    res2 << StemCountId.new(name, 0, id)
+  end
+
+  res2
+end
+
+def insert_stems_into_db(stems, page_id)
+  #GRANT ALL ON bayesfortest.* TO 'testuser'@'localhost' IDENTIFIED BY 'test';
+  dbh = Mysql.real_connect("localhost", "testuser", "test", "bayesfortest")
+
+  #insert into sources (name) values ("news.google.com");
+  #insert into pages (source_id, url) values (1, 'http://news.google.com');
+
+  res = dbh.prepare "INSERT INTO words (id, page_id, scantime, count) VALUES (?, ?, ?, ?)"
   stems.each do |stem|
-    res.execute stem.stem, stem.count, Time.now
+    res.execute stem.id, page_id, Time.now, stem.count
   end
 
   res.close
