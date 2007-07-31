127421dca0d1474020e227c4e2d1733992febf1d
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2007-07-31 20:18:46 +0000

    * python/website.py: added sources and pages * python/templates/selectall.html: added a new template   that prints 'select * from X' in a html table

--- prototype1/python/website.py
+++ prototype1/python/website.py
@@ -6,6 +6,8 @@ render = web.template.render('templates/')
 urls = (
   '/', 'index',
   '/words', 'words',
+  '/pages', 'pages',
+  '/sources', 'sources',
   '/int_words', 'int_words',
   '/addword', 'addword',
   '/addword2', 'addword2'
@@ -15,15 +17,28 @@ class index:
     def GET(self):
         print "<h1>bayes-swarm test</h1>"
         print "<a href='/words'>words</a><br>"
-        print "<a href='/addword'>add word</a><br>"
-        print "<a href='/int_words'>list interesting words</a><br>"
+        print "<a href='/pages'>pages</a><br>"
+        print "<a href='/sources'>sources</a><br>"
+        print "<a href='/int_words'>list interesting words</a> (<a href='/addword'>add word</a>)<br>"
 
 class words:
     def GET(self):
-        # FIXME: join words with int_words so that we have stems too
-        stems = web.select('words')
+        join_words_intwords = """SELECT a.id, b.name, page_id, scantime, count, titlecount, weight
+                                 FROM words a, int_words b 
+                                 WHERE a.id=b.id"""
+        stems = web.query(join_words_intwords) 
         print render.words(stems, cache=False)
 
+class pages:
+    def GET(self):
+        pages = web.select('pages')
+        print render.selectall(pages)
+
+class sources:
+    def GET(self):
+        sources = web.select('sources')
+        print render.selectall(sources)
+            
 class int_words:
     def GET(self):
         stems = web.select('int_words')
