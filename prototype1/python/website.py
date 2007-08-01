fe47b620c7ef26d9d6b33101f8533ac860775c38
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2007-08-01 19:58:17 +0000

    * prototype1/python/templates/selectall.html: fixed column heading with more than one record * prototype1/python/website.py: added own_query page where user can provided own query

--- prototype1/python/website.py
+++ prototype1/python/website.py
@@ -11,6 +11,8 @@ urls = (
   '/sources', 'sources',
   '/int_words', 'int_words',
   '/addword', 'addword',
+  '/most_3_words', 'most_3_words',
+  '/own_query', 'own_query'
 )
 
 class index:
@@ -18,10 +20,18 @@ class index:
         web.header("Content-Type","text/html; charset=utf-8") 
 #        print "<html><head><title>wiki pages</title></head><body>"
         print "<h1>bayes-swarm test</h1>"
+        print "<h2>tables</h2>"
         print "<a href='words'>words</a><br>"
         print "<a href='pages'>pages</a><br>"
         print "<a href='sources'>sources</a><br>"
         print "<a href='int_words'>list interesting words</a> (<a href='addword'>add word</a>)<br>"
+
+        print "<h2>queries</h2>"
+        print "<a href='most_3_words'>most 3 popular words with date and page</a><br>"        
+        print "<a href='own_query'>my own query</a><br>"
+        
+        print "<h2>graphs</h2>"
+        print "TODO"
 #        print "</body></html>"
 
 class words:
@@ -53,8 +63,27 @@ class addword:
 
     def POST(self):
         i = web.input()
-        n = web.insert('int_words', name=i.title)
-        web.seeother('/')
+        web.insert('int_words', name=i.title)
+        web.seeother('./')
+
+class most_3_words:
+    def GET(self):
+        most_3_words_query = '''SELECT a.id, b.name, c.url, date(scantime) as date, count 
+                                FROM words a, int_words b, pages c 
+                                WHERE a.id=b.id and a.page_id=c.id 
+                                GROUP BY a.id, b.name 
+                                ORDER BY count desc LIMIT 3;'''
+        res = web.query(most_3_words_query)                                 
+        print render.selectall(res)
+
+class own_query:
+    def GET(self):
+        print render.own_query()
+
+    def POST(self):
+        i = web.input()
+        res = web.query(i.postarea)
+        print render.selectall(res)
 
 if __name__ == "__main__":
     web.config.db_parameters = dict(dbn='mysql', user='testuser', pw='test', db='bayesfortest')
