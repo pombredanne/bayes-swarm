ef277e81db7c5867a193b9cfd0ecedc33bc2683e
a.bonazzi <a.bonazzi@600039af-b535-0410-96a4-311d76c0c601>
2007-09-08 19:08:48 +0000

    speriamo bene

--- trunk/prototype1/ruby/swarm_shoal.rb
+++ trunk/prototype1/ruby/swarm_shoal.rb
@@ -12,7 +12,5 @@ interesting_stems = get_interesting_stems()
 
 for page in pages
     counted_stems = swarm_extract(page.url, sourcetype, notidy, interesting_stems)
-    if ( counted_stems != nil )
-      insert_stems_into_db(counted_stems, page.id)
-    end
+    insert_stems_into_db(counted_stems, page.id)
 end
--- trunk/prototype1/ruby/update_support.rb
+++ trunk/prototype1/ruby/update_support.rb
@@ -5,7 +5,7 @@ require 'mysql'
 
 def load_pages(s_id, source)
 
-   dbh = Mysql.real_connect("localhost", "testuser", "test", "bayesfortest")
+   dbh = Mysql.real_connect("localhost", "testuser", "test", "bayesfor")
 
    content = "" # raw content of rss feed will be loaded here
    open(source) do |s| content = s.read end
@@ -20,21 +20,24 @@ def load_pages(s_id, source)
    count = 0
 
    res = dbh.prepare "INSERT INTO pages ( source_id, url ) VALUES ( ?, ? )"
-
+   chk = dbh.prepare "SELECT id FROM pages WHERE url = ? "
    rss.items.size.times do
        page   = rss.items[count].link
-       print page, "\n"
-       count +=1
-
-       res.execute s_id , page
+       hold   = chk.execute page
+       hold   = hold.fetch
+       if hold == nil 
+          print page, "\n"
+          count +=1
+          res.execute s_id , page
+       end
    end
    res.close
-
+   chk.close
 end
 
 def get_sources()
 
-    dbh = Mysql.real_connect("localhost", "testuser", "test", "bayesfortest")
+    dbh = Mysql.real_connect("localhost", "testuser", "test", "bayesfor")
     sou = dbh.query("SELECT id, name FROM sources")
     sources = Array.new
     while row = sou.fetch_row do
