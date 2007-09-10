2751b43f613948054aec3aa4a21fbd4191338cc8
a.bonazzi <a.bonazzi@600039af-b535-0410-96a4-311d76c0c601>
2007-09-10 16:44:20 +0000

    This is working-progress release for version 0.2 The table "articles" was added to the database. All the relevant variables in the swarm code are renamed accordingly.
    So far, the table "pages" is a copy of "sources". This will be
    modified in the next release.

--- db/schema_release-0.2.sql
+++ db/schema_release-0.2.sql
@@ -1,3 +1,9 @@
+#####
+# db definition for release 0.2
+# it works, but need to be refined
+
+
+
 drop database if exists bayesfortest ;
 
 create database bayesfortest ;
@@ -36,11 +42,27 @@ CREATE
         id int(11) NOT NULL AUTO_INCREMENT,
         source_id int(11) NOT NULL,
         url varchar(255) NOT NULL,
-        flag int(1) NOT NULL,
-	    language varchar(3) NOT NULL,
+	language varchar(3) NOT NULL,
         PRIMARY KEY USING BTREE(id),
         constraint fk_page_source foreign key(source_id) references sources(id),
-	    constraint fk_page_lang   FOREIGN key(language)  references sources(language)
+        constraint fk_page_lang   FOREIGN key(language)  references sources(language)
+    )
+    ENGINE=InnoDB;
+
+
+
+
+CREATE
+    TABLE articles
+    (
+        id int(11) NOT NULL AUTO_INCREMENT,
+        page_id int(11) NOT NULL,
+        url varchar(255) NOT NULL,
+	     language varchar(3) NOT NULL,
+        flg_visited int(1) NOT NULL,
+        PRIMARY KEY USING BTREE(id),
+        constraint fk_article_page foreign key(page_id) references pages(id),
+          constraint fk_article_lang   FOREIGN key(language)  references pages(language)
     )
     ENGINE=InnoDB;
 
@@ -61,16 +83,16 @@ CREATE
     TABLE words
     (
         id int(11) NOT NULL,
-        page_id int(11) NOT NULL,
+        article_id int(11) NOT NULL,
         scantime DATETIME NOT NULL,
         count int(11) NOT NULL DEFAULT 0,
         titlecount int(11) NOT NULL DEFAULT 0,
         weight decimal(6,3) NOT NULL DEFAULT 0.0,
-	    language varchar(3) NOT NULL,
-        PRIMARY KEY USING BTREE(id, page_id, scantime),
+        language varchar(3) NOT NULL,
+        PRIMARY KEY USING BTREE(id, article_id, scantime),
         constraint fk_word_intword foreign key(id) references int_words(id),
-        constraint fk_word_page foreign key(page_id) references pages(id),
-	    constraint fk_word_lang FOREIGN key(language) references pages(language)
+        constraint fk_word_article foreign key(article_id) references articles(id),
+        constraint fk_word_lang FOREIGN key(language) references articles(language)
     )
     ENGINE=InnoDB;
 
--- prototype1/ruby/load_sources.rb
+++ prototype1/ruby/load_sources.rb
@@ -5,6 +5,10 @@ require 'mysql'
 
 dbh = Mysql.real_connect("localhost", "testuser", "test", "bayesfortest")
 sou = dbh.prepare "INSERT INTO sources ( name, language ) values ( ? , ?)"
+get = dbh.prepare "SELECT id FROM sources WHERE name = ? "
+pag = dbh.prepare "INSERT INTO pages ( source_id, url, language ) values ( ?, ?, ? ) "
+
+
 
 sources = Array.new
 sources[0]="http://www.corriere.it/rss/homepage.xml"
@@ -16,6 +20,10 @@ sources[4]="http://www.gazzetta.it/rss/Home.xml"
 
 sources.each do | source |
    sou.execute source, 'ita'
+   hold=get.execute source
+   hold=hold.fetch
+   print hold
+   pag.execute hold[0], source, "ita"
 end
 
 
