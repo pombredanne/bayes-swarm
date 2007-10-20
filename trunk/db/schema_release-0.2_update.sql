1323fbfd1cb17320dfadfeae832d14a720bd906a
battlehorse <battlehorse@600039af-b535-0410-96a4-311d76c0c601>
2007-10-20 20:24:44 +0000

    Updated schema to correct AR mappings

--- trunk/db/schema_release-0.2_update.sql
+++ trunk/db/schema_release-0.2_update.sql
@@ -1,12 +1,21 @@
 CREATE
-    TABLE types
+    TABLE kinds
     (
-        type varchar(3) NOT NULL,
-        PRIMARY KEY USING BTREE(type)
+		id int(11) NOT NULL AUTO_INCREMENT,
+        kind varchar(3) NOT NULL,
+        PRIMARY KEY USING BTREE(id)
     )    
     ENGINE=InnoDB;
 
-ALTER TABLE pages ADD COLUMN page_type varchar(3) NOT NULL;
-ALTER TABLE pages ADD constraint fk_page_type foreign key(page_type) references types(type);
+INSERT INTO kinds (id,kind) values (1,'url'),(2,'rss');
+
+ALTER TABLE pages ADD COLUMN kind_id int(11) NOT NULL;
+ALTER TABLE pages ADD constraint fk_page_kind foreign key(kind_id) references kinds(id);
 ALTER TABLE pages ADD COLUMN last_scantime DATETIME NOT NULL;
-UPDATE pages SET type="url";
+UPDATE pages SET kind_id = 1;
+
+CREATE
+	VIEW extended_words AS
+	SELECT w.* , iw.name , iw.language_id
+	FROM words w , int_words iw
+	WHERE w.id = iw.id ;
