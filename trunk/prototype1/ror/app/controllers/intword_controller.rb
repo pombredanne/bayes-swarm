ec637768fcc42ad170d00cadd3373113fd2cda9b
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2007-11-01 18:14:37 +0000

    * intword/show now plots the stem 3m time series

--- trunk/prototype1/ror/app/controllers/intword_controller.rb
+++ trunk/prototype1/ror/app/controllers/intword_controller.rb
@@ -19,4 +19,35 @@ class IntwordController < ApplicationController
   def cloud
     @intwords = Intword.find_popular(1)
   end
+
+  def demogruff
+    require 'gruff'
+    iw = Intword.find(@params["id"])
+    
+    g = Gruff::Line.new(480)
+    g.title = iw.name
+
+    tseries = iw.intword_time_series.find(:all, :conditions=>"date>'#{Date.today()<<3}'", :order=>"date")
+    data = Array.new()
+    labels = Array.new()
+    tseries.each do |ts|
+      data << ts.count
+      #labels << ts.date
+    end
+
+    g.data(iw.name, data)
+    #g.data("Oranges", [4, 8, 7, 9, 8, 9])
+    #g.data("Watermelon", [2, 3, 1, 5, 6, 8])
+    #g.data("Peaches", [9, 9, 10, 8, 7, 9])
+
+    #g.labels = {0 => '2003', 2 => '2004', 4 => '2005'}
+
+    filename = "tmp/cache/intword_ts_#{iw.id}.png"
+    
+    # this writes the file to the hard drive for caching
+    # and then writes it to the screen.
+  
+    g.write(filename)
+    send_file filename, :type => 'image/png', :disposition => 'inline'
+  end
 end
