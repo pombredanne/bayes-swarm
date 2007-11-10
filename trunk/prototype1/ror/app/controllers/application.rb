3bfed98225ae992ae4056926e9303b1c6d9dc4b9
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2007-11-10 10:35:19 +0000

    * added language support trough globalize (first draft) * flags added on top of standard template for lang selection * routes.rb modified accordingly * assumes that intword has a foreign key in globalize_languages

--- trunk/prototype1/ror/app/controllers/application.rb
+++ trunk/prototype1/ror/app/controllers/application.rb
@@ -4,4 +4,15 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_website_session_id'
+  
+  before_filter :set_locale
+
+  def set_locale
+    if !params[:locale].nil? && LOCALES.keys.include?(params[:locale])
+      Locale.set LOCALES[params[:locale]]
+    else
+      redirect_to params.merge( 'locale' => Locale.base_language.code )
+    end
+  end
+    
 end
--- trunk/prototype1/ror/app/models/intword.rb
+++ trunk/prototype1/ror/app/models/intword.rb
@@ -68,3 +68,4 @@ class IntwordTimeSeries
   # FIXME: add a "kind of intersection" method to join single time series
   # for multivariate plots
 end
+
