5c76b0ef7160162297ca1ff26d5ccb2f39e36d67
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2007-11-11 16:58:49 +0000

    * whoops, forgot to add _form.rhtml

--- db/schema_users.sql
+++ db/schema_users.sql
@@ -1,6 +1,6 @@
 # contains users
 # useful for keeping track of who can access the db
 
-GRANT ALL ON bayesswarm02.* TO 'testuser'@'localhost' IDENTIFIED BY 'test';
-GRANT SELECT ON bayesswarm02.* TO 'webuser'@'localhost' IDENTIFIED BY 'test';
-GRANT INSERT ON bayesswarm02.int_words TO 'webuser'@'localhost' IDENTIFIED BY 'test';
+GRANT ALL ON bayesswarmdev.* TO 'testuser'@'localhost' IDENTIFIED BY 'test';
+GRANT SELECT ON bayesswarmdev.* TO 'webuser'@'localhost' IDENTIFIED BY 'test';
+GRANT INSERT ON bayesswarmdev.intwords TO 'webuser'@'localhost' IDENTIFIED BY 'test';
