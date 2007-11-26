cd24a49538bf228024f4b44fb61d54dc3f744335
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2007-11-26 22:50:26 +0000

    * homepage fully translated

--- db/schema_users.sql
+++ db/schema_users.sql
@@ -1,7 +1,7 @@
 # contains users
 # useful for keeping track of who can access the db
 
-GRANT ALL ON bayesswarmdev.* TO 'testuser'@'localhost' IDENTIFIED BY 'test';
-GRANT SELECT ON bayesswarmdev.* TO 'webuser'@'localhost' IDENTIFIED BY 'test';
-GRANT INSERT ON bayesswarmdev.intwords TO 'webuser'@'localhost' IDENTIFIED BY 'test';
-GRANT INSERT, UPDATE ON bayesswarmdev.globalize_translations TO 'webuser'@'localhost' IDENTIFIED BY 'test';
+GRANT ALL ON bayesswarm021.* TO 'testuser'@'localhost' IDENTIFIED BY 'test';
+GRANT SELECT ON bayesswarm021.* TO 'webuser'@'localhost' IDENTIFIED BY 'test';
+GRANT INSERT ON bayesswarm021.intwords TO 'webuser'@'localhost' IDENTIFIED BY 'test';
+GRANT INSERT, UPDATE ON bayesswarm021.globalize_translations TO 'webuser'@'localhost' IDENTIFIED BY 'test';
