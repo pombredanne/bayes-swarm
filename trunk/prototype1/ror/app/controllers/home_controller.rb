28b6495c03203eed55426c8931152074f201c622
a.bonazzi <a.bonazzi@600039af-b535-0410-96a4-311d76c0c601>
2007-12-28 20:19:39 +0000

    New US Primary 2008 home page    - user selection of parties    - support of pies and plots

--- trunk/prototype1/ror/app/controllers/home_controller.rb
+++ trunk/prototype1/ror/app/controllers/home_controller.rb
@@ -19,27 +19,52 @@ class HomeController < ApplicationController
 
   def usprimary2008
     # def interesting words for usprimary2008
-    @candidate_id = "330-333-321-332-335"
-    intword_ids = @candidate_id.split("-")
-    
-    @candidate_series = Array.new() 
-    @candidate_names  = Array.new()
-    count = 0
-    intword_ids.each do |iw_id|
-      
-      iw = Intword.find(iw_id)
-      begin
-        @candidate_series[count] = iw.get_time_series("3w").values
-        @candidate_names[count] = iw.name
-      rescue RuntimeError
-        #return nil
-      end
-      count += 1
+    @candidate={"all" => "330-331-333-321-332", 
+       "rep" => "333-335-340-341-334-347",
+       "dem" => "330-321-332-339-338-348-346-337-336" }
+    @candidate={"all" => "330-331-333-321-332", 
+            "rep" => "333-335-340-341-331",
+            "dem" => "321-330-332-348-338" }
+    if ( params[:id] == nil )
+      params[:id] = @candidate["rep"]
     end
+    
+    
+   
+    @intword_ids = params[:id].split("-")
+    
+#    @candidate_series = Array.new() 
+#    @candidate_names  = Array.new()
+#    count = 0
+#    intword_ids.each do |iw_id|
+      
+#      iw = Intword.find(iw_id)
+#      begin
+#        @candidate_series[count] = iw.get_time_series("3w").values
+#        @candidate_names[count] = iw.name
+#      rescue RuntimeError
+#        #return nil
+#      end
+#      count += 1
+#    end
         
 
   end
-  
+    def usprimary2008_timeview
+      # def interesting words for usprimary2008
+      @candidate={"all" => "330-331-333-321-332", 
+         "rep" => "333-335-340-341-334-347",
+         "dem" => "330-321-332-339-338-348-346-337-336" }
+      @candidate={"all" => "330-331-333-321-332", 
+              "rep" => "333-335-340-341-331",
+              "dem" => "321-330-332-348-338" }
+      if ( params[:id] == nil )
+        params[:id] = @candidate["rep"]
+      end
+
+
+
+    end 
 end
 
 class SpecialReportController < ApplicationController
