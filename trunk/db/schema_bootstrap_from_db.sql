39acd221d8d9e496a11e0b5f19db82bc8a77c11e
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2008-08-15 08:30:30 +0000

    added sql schema for easily bootstrap a new db from an old one

---
+++ trunk/db/schema_bootstrap_from_db.sql
@@ -0,0 +1,9 @@
+
+insert into pulsar.globalize_languages select * from bayesswarm022.globalize_languages;
+insert into pulsar.globalize_countries select * from bayesswarm022.globalize_countries;
+insert into pulsar.globalize_translations select * from bayesswarm022.globalize_translations;
+
+insert into pulsar.kinds select * from bayesswarm022.kinds;
+
+insert into pulsar.sources (id, name, created_at) select id, name, "2008-08-14 15.41" from bayesswarm022.sources;
+insert into pulsar.pages (id, source_id, url, last_scantime, kind_id, language_id, created_at) select id, source_id, url, NULL, kind_id, language_id, "2008-08-14 15.41" from bayesswarm022.pages;
