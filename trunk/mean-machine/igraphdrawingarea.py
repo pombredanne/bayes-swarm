ada74bf7c52817bca3d09ef99c148d13ebdfdd42
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2008-12-12 17:28:53 +0000

    [mean-machine]
    * removed bs-xapian-nearest-keywords.py since obsolete
    * added bs-nearest-plotter.py
    * minor fixes in bs-xapian-nearest.py

---
+++ trunk/mean-machine/igraphdrawingarea.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# 
+# Mean Machine: subclass gtk.DrawingArea in order to plot a graph
+#
+# == Author
+# Matteo Zandi [matteo.zandi@bayesfor.eu]
+#
+# == Copyright
+# Copyright(c) 2008 - bayes-swarm project.
+# Licensed under the Apache2 License.
+# USA
+
+import gtk
+from gtk import gdk
+import cairo
+import igraph
+
+class IGraphDrawingArea(gtk.DrawingArea):
+    def __init__(self, graph):
+        gtk.DrawingArea.__init__(self)
+        self.set_size_request(300, 300)
+        self.connect("expose_event", self.expose)
+        self.g = graph
+
+    def expose(self, widget, event):
+        context = widget.window.cairo_create()
+
+        # set a clip region for the expose event
+        context.rectangle(event.area.x, event.area.y,
+                          event.area.width, event.area.height)
+        context.clip()
+        self.draw(context)
+
+        return False
+
+    def draw(self, context):
+        rect = self.get_allocation()
+
+        surface = cairo.ImageSurface (cairo.FORMAT_ARGB32,
+                        rect.width,
+                        rect.height)
+
+        plot = igraph.drawing.Plot(surface, (0, 0, rect.width, rect.height))
+        #plot.add(self.g, layout="drl", margin=(20,20,20,20), vertex_size=self.g.vs['sizes'], fixed = self.g.vs['fixed'], seed = self.g.vs['fr_seed_coords'])
+        plot.add(self.g, layout="fr", margin=(20,20,20,20), vertex_size=self.g.vs['sizes']) #, fixed = self.g.vs['fixed'], seed = self.g.vs['fr_seed_coords'])
+        plot.redraw()
+
+        context.set_source_surface (surface)
+        context.paint()
+        
+        return False
+
+    def redraw_canvas(self):
+        if self.window:
+            alloc = self.get_allocation()
+            rect = gdk.Rectangle(alloc.x, alloc.y, alloc.width, alloc.height)
+            self.window.invalidate_rect(rect, True)
+            self.window.process_updates(True)
+
+    def change_graph(self, g):
+        self.g = g
+        self.redraw_canvas()
+
+class Demo():
+    def __init__(self):
+        window = gtk.Window()
+        vbox = gtk.VBox(False, 0)
+        
+        g = igraph.Graph.Barabasi(10, 15, directed=False)
+        
+        self.igraph_drawing_area = IGraphDrawingArea(g)
+        vbox.pack_start(self.igraph_drawing_area, True, True, 0)
+
+        adj = gtk.Adjustment(10, 1, 100, 1, 1, 0)
+        adj.connect("value_changed", self.cb_value_changed)
+        self.slider = gtk.HScale(adj)
+        self.slider.set_digits(0)
+        vbox.pack_start(self.slider, False, False, 0)
+        
+        window.add(vbox)
+        window.connect("destroy", gtk.main_quit)
+        window.show_all()
+
+        gtk.main()
+
+    def cb_value_changed(self, adj):
+        g = igraph.Graph.Barabasi(int(adj.value), 15, directed=False)
+        self.igraph_drawing_area.change_graph(g)
+
+if __name__ == "__main__":
+    Demo()
