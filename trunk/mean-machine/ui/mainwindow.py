b61a53f099ad6bfb9e35f423a31678a481d6b06d
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2008-12-31 15:17:27 +0000

    [mean-machine] * better remote support in user interface

--- trunk/mean-machine/ui/mainwindow.py
+++ trunk/mean-machine/ui/mainwindow.py
@@ -17,27 +17,28 @@ format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(level=logging.DEBUG, format=format)
 logging = logging.getLogger('ui.mainwindow')
 
+MODEL_DB_URL, MODEL_DB_PORT, MODEL_DB_IS_LOCAL, MODEL_DB_VALIDITY = range(4)
+FLAG_DB_NOT_CHECKED, FLAG_DB_IS_VALID, FLAG_DB_IS_NOT_VALID = [gtk.STOCK_DIALOG_QUESTION, gtk.STOCK_YES, gtk.STOCK_NO]
+
 class MMSearchForm(gtk.VBox):
     def __init__(self):
         gtk.VBox.__init__(self, False, 6)
-        
+
         self.upperbox = gtk.HBox(False, 12)
         self.set_border_width(6)
-        
+
         self.entry = gtk.Entry()
 
         self.start_button = gtk.Button()
         self.start_button.set_label('Search')
         self.start_button.set_flags(gtk.CAN_DEFAULT)
         # TODO: set grab_default() when it is packed into the window
-                
+
         self.combobox = gtk.combo_box_new_text()
         self.combobox.append_text('it')
         self.combobox.append_text('en')
         self.combobox.set_active(0)
 
-        self.selected_language = 'it'
-        
         self.progressbar = gtk.ProgressBar()
         self.progressbar.set_size_request(100,-1)
 
@@ -47,41 +48,86 @@ class MMSearchForm(gtk.VBox):
         self.upperbox.pack_start(gtk.Label("Language:"), False, False, 0)
         self.upperbox.pack_start(self.combobox, False, False, 0)
         self.upperbox.pack_start(self.progressbar, False, False, 0)
-        
+
         hbox2 = gtk.HBox(False, 12)
-        
-        self.comboboxentry_db = gtk.combo_box_entry_new_text()
-        self.comboboxentry_db.append_text('/home/matteo/Development/pagestore/renzi_xap_20081220')
-        self.comboboxentry_db.append_text('/home/matteo/Development/pagestore/us2008_xap')
+
+        def set_local_or_remote(tvcolumn, cell, model, iter):
+            is_local = model.get_value(iter, 0)
+            if is_local == True:
+                label = 'local'
+            else:
+                label = 'remote'
+            cell.set_property('text', label)
+            return
+
+        # combobox for selecting local/remote db
+        model = gtk.ListStore('gboolean')
+        self.combobox_dblocal = gtk.ComboBox(model)
+        cell = gtk.CellRendererText()
+        self.combobox_dblocal.pack_start(cell, True)
+        self.combobox_dblocal.set_cell_data_func(cell, set_local_or_remote)
+        model.append([True])
+        model.append([False])
+        self.combobox_dblocal.set_active(0)
+
+        # db comboboxentry
+        # model = MODEL_DB_URL, MODEL_DB_PORT, MODEL_DB_IS_LOCAL, MODEL_DB_VALIDITY
+        # MODEL_DB_VALIDITY = FLAG_DB_NOT_CHECKED, FLAG_DB_IS_VALID, FLAG_DB_IS_NOT_VALID
+        self.model_db = gtk.ListStore(str, int, 'gboolean', str)
+        self.filtered_model_db = self.model_db.filter_new()
+        self.comboboxentry_db = gtk.ComboBoxEntry(self.filtered_model_db)
+        cellpb = gtk.CellRendererPixbuf()
+        self.comboboxentry_db.pack_start(cellpb, False)
+        self.comboboxentry_db.add_attribute(cellpb, 'stock_id', MODEL_DB_VALIDITY)
+        #cell = gtk.CellRendererText()
+        #self.comboboxentry_db.pack_start(cell, True)
+        #self.comboboxentry_db.add_attribute(cell, 'text', MODEL_DB_IS_LOCAL)
+        self.model_db.append(['/home/matteo/Development/pagestore/renzi_xap_20081220', 0, True, FLAG_DB_NOT_CHECKED])
+        self.model_db.append(['/home/matteo/Development/pagestore/us2008_xap', 0, True, FLAG_DB_NOT_CHECKED])
+        self.model_db.append(['10.0.2.2:3333', 0, False, FLAG_DB_NOT_CHECKED])
+        self.model_db.append(['localhost:3333', 0, False, FLAG_DB_NOT_CHECKED])
         self.comboboxentry_db.set_active(0)
-        # TODO: use a more complex liststore in order to point out
-        # valid databases when the user opens the combobox drop down
-        
+
+        self.image_connected = gtk.Image()
+
+        self.connect_button = gtk.Button()
+        self.connect_button.set_label('Connect')
+
         hbox2.pack_start(gtk.Label('Database:'), False, False, 0)
+        hbox2.pack_start(self.combobox_dblocal, False, False, 0)
         hbox2.pack_start(self.comboboxentry_db, True, True, 0)
-        
+        hbox2.pack_start(self.image_connected, False, False, 0)
+        hbox2.pack_start(self.connect_button, False, False, 0)
+
         self.pack_start(self.upperbox, False, False, 0)
         self.pack_start(hbox2, False, False, 0)
 
 class MMMainFrame(gtk.VBox):
     def __init__(self, component):
         gtk.VBox.__init__(self)
-        
+
         self.resultbox = gtk.VBox(False, 0)
         self.searchform = MMSearchForm()
         self.set_component(component)
-        
+
         self.pack_start(self.resultbox, True, True, 0)
         self.pack_start(self.searchform, False, False, 0)
 
-        self.searchform.combobox.connect('changed', self.on_lang_selected)
-        self.searchform.comboboxentry_db.connect('changed', self.on_db_selected)
-        self.searchform.comboboxentry_db.child.connect('changed', self.on_db_manually_entered, self.searchform.comboboxentry_db)
-        self.searchform.start_button.connect('clicked', self.on_start_button_clicked)
-        
-        # assign self.selected_language and self.db
-        self.on_db_selected(self.searchform.comboboxentry_db)
+        # assign self.selected_language, self.selected_localdb
         self.on_lang_selected(self.searchform.combobox)
+        self.on_dblocal_changed(self.searchform.combobox_dblocal)
+
+        self.searchform.combobox.connect('changed', self.on_lang_selected)
+        self.searchform.combobox_dblocal.connect('changed', self.on_dblocal_changed)
+        self.searchform.comboboxentry_db.connect('changed', self.on_db_selected)
+        self.searchform.comboboxentry_db.child.connect('changed', self.on_db_manually_entered)
+        self.searchform.start_button.connect('clicked', self.on_start_button_clicked)
+        self.searchform.connect_button.connect('clicked', self.on_connect_button_clicked)
+        self.searchform.filtered_model_db.set_visible_func(self.visible_dbs_cb)
+        self.searchform.filtered_model_db.refilter()
+
+        # assign self.db
+        self.on_db_selected(self.searchform.comboboxentry_db, self.selected_localdb)
 
     def set_component(self, component):
         self.component = component()
@@ -92,57 +138,122 @@ class MMMainFrame(gtk.VBox):
         index = combobox.get_active()
         self.selected_language = model[index][0]
 
-    def db_is_valid(self, entered_db):
-        'returns True if db can be opened by Xapian'
+    def visible_dbs_cb(self, model, iter):
+        logging.debug('Is db %s local? %s, therefore is db shown? %s' % (model.get_value(iter, MODEL_DB_URL), model.get_value(iter, MODEL_DB_IS_LOCAL), model.get_value(iter, MODEL_DB_IS_LOCAL) == self.selected_localdb))
+        return model.get_value(iter, MODEL_DB_IS_LOCAL) == self.selected_localdb
+
+    def on_dblocal_changed(self, combobox):
+        model = combobox.get_model()
+        index = combobox.get_active()
+        self.selected_localdb = model[index][0]
+        logging.debug('Selecting local db? %s' % self.selected_localdb)
+        self.searchform.connect_button.set_sensitive(not self.selected_localdb) # set sensitive only if remote selected
+        self.searchform.filtered_model_db.refilter() # show only local/remote dbs in comboboxentry
+
+        model = self.searchform.comboboxentry_db.get_model()
+        iter = model.get_iter_root()
+        while (iter):
+            # FIXME: this implies that entries both local and remote are always present
+            if model.get_value(iter, MODEL_DB_IS_LOCAL) == self.selected_localdb:
+                #print self.searchform.model_db.get_value(iter, MODEL_DB_URL)
+                break
+            iter = model.iter_next(iter)
+        self.searchform.comboboxentry_db.set_active(model.get_path(iter)[0])
+
+    def is_db_valid(self, entered_db):
+        # - returns True if db can be opened by Xapian
+        # - changes image_connected
         try:
-            if ':' not in entered_db:
+            logging.debug('Checking %s' % entered_db)
+            if self.selected_localdb:
                 self.db = xapian.Database(entered_db)
             else:
                 db_host, port = entered_db.split(':')
                 self.db = xapian.remote_open(db_host, int(port))
         except xapian.DatabaseOpeningError, e:
             logging.error('Error while opening %s (%s)' % (entered_db, e))
+            self.searchform.image_connected.set_from_stock(gtk.STOCK_DISCONNECT, gtk.ICON_SIZE_MENU)
             return False
         except xapian.NetworkError, e:
             logging.error('Error while opening %s (%s)' % (entered_db, e))
+            self.searchform.image_connected.set_from_stock(gtk.STOCK_DISCONNECT, gtk.ICON_SIZE_MENU)
             return False
+        #except:
+        #    logging.error('Error while opening %s' % entered_db)
+        #    self.searchform.image_connected.set_from_stock(gtk.STOCK_DISCONNECT, gtk.ICON_SIZE_MENU)
+        #    return False
         else:
+            self.searchform.image_connected.set_from_stock(gtk.STOCK_CONNECT, gtk.ICON_SIZE_MENU)
             return True
-            
-    
-    def add_db_to_combobox(self, combobox, entered_db):
-        'adds user specified db to combobox only if not already present'
-        model = combobox.get_model()
+
+    def add_db_to_model(self, model, entered_db):
+        'adds user specified db to model only if not already present'
         iter = model.get_iter_root()
         unique = True
         while (iter):
-            unique = unique and model.get_value(iter, 0) != entered_db
+            unique = unique and model.get_value(iter, MODEL_DB_URL) != entered_db
             iter = model.iter_next(iter)
         if unique == True:
-            combobox.append_text(entered_db)
-    
-    def on_db_selected(self, combobox):
+            model.append([entered_db, 0, self.selected_localdb, FLAG_DB_IS_VALID])
+
+    def check_db_if_needed(self, combobox, do_check, entry=None):        
+        #model = self.searchform.model_db
         model = combobox.get_model()
         index = combobox.get_active()
-        selected_db = model[index][0]
-        # check if db is valid and disable search button accordingly
-        if self.db_is_valid(selected_db):
-            self.selected_db = selected_db
-            self.searchform.upperbox.set_sensitive(True)
-        else:
-            self.searchform.upperbox.set_sensitive(False)
 
-    def on_db_manually_entered(self, entry, combobox):
-        entered_db = entry.get_text()
-        if self.db_is_valid(entered_db):
-            self.add_db_to_combobox(combobox, entered_db)
-            self.searchform.upperbox.set_sensitive(True)
+        # if we're editing comboboxentry set MODEL_DB_IS_LOCAL = self.selected_localdb
+        if index == -1:
+            logging.debug("Running 'check_db_if_needed', triggered by manual insertion (entry=%s)." % entry)
+            selected_db_is_local = self.selected_localdb
+            if entry is not None:
+                db_url = entry.get_text()
+            else:
+                return
+        else:
+            db_url = model[index][MODEL_DB_URL]
+            selected_db_is_local = model[index][MODEL_DB_IS_LOCAL]
+            logging.debug("Running 'check_db_if_needed' on %s, triggered by combobox selection (entry=%s)." % (db_url, entry))
+
+        # Avoid stupid timeouts, check only if we pass True to do_check
+        # or local is selected. In any case, check only current type
+        # (local or remote) of dbs
+        logging.debug('index: %d, do_check: %s, selected_localdb: %s, is_local: %s' % (index, do_check, self.selected_localdb, selected_db_is_local))
+        if (do_check or self.selected_localdb) and (selected_db_is_local == self.selected_localdb):
+            # check if db is valid and disable search button accordingly
+            if self.is_db_valid(db_url):
+                self.selected_db = db_url
+                if index == -1:
+                    self.add_db_to_model(self.searchform.model_db, db_url)
+                else:
+                    #model[index][MODEL_DB_VALIDITY] = FLAG_DB_IS_VALID
+                    iter_filtered_model = model.get_iter((index,))
+                    iter_full_model = model.convert_iter_to_child_iter(iter_filtered_model)
+                    self.searchform.model_db.set_value(iter_full_model, MODEL_DB_VALIDITY, FLAG_DB_IS_VALID)
+                self.searchform.upperbox.set_sensitive(True)
+            else:
+                if index == -1:
+                    #model[index][MODEL_DB_VALIDITY] = FLAG_DB_IS_NOT_VALID
+                    iter = model.get_iter((index,))
+                    self.searchform.model_db.set_value(iter, MODEL_DB_VALIDITY, FLAG_DB_IS_NOT_VALID)
+                self.searchform.upperbox.set_sensitive(False)
         else:
             self.searchform.upperbox.set_sensitive(False)
-            
+            self.searchform.image_connected.set_from_stock(gtk.STOCK_DISCONNECT, gtk.ICON_SIZE_MENU)
+
+    def on_db_selected(self, combobox, do_check=False):
+        self.check_db_if_needed(combobox, do_check, None)
+
+    def on_db_manually_entered(self, entry, do_check=False):
+        self.check_db_if_needed(self.searchform.comboboxentry_db, do_check, entry)
+
     def on_start_button_clicked(self, button):
         self.searchform.set_sensitive(False)
         self.refresh_results()
+
+    def on_connect_button_clicked(self, button):
+        # TODO: set_sensitive(False) on remote/local combo + db combo
+        # trigger db check (force avoidcheck as True)
+        self.on_db_manually_entered(self.searchform.entry, True)
 
     def refresh_results(self):
         stemmer = xapian.Stem(self.selected_language)
@@ -158,9 +269,9 @@ class MMMainFrame(gtk.VBox):
         query1 = qp.parse_query(self.searchform.entry.get_text(), xapian.QueryParser.FLAG_BOOLEAN)
         query2 = xapian.Query(xapian.Query.OP_VALUE_RANGE, 0, self.selected_language, self.selected_language)
         query = xapian.Query(xapian.Query.OP_AND, query1, query2)
-        
+
         logging.debug("Setting query: %s" % query.get_description())
-        
+
         enquire = xapian.Enquire(self.db)
         enquire.set_query(query)
 
@@ -170,10 +281,10 @@ class MMMainFrame(gtk.VBox):
 class MainMachine(object):
     def __init__(self):
         components = get_components()
-        
+
         w = gtk.Window()
         w.connect('destroy', gtk.main_quit)
-        w.set_size_request(600, 600)
+        w.set_size_request(700, 600)
         #w.set_border_width(12)
 
         hbox = gtk.HBox(False, 12)
@@ -186,10 +297,10 @@ class MainMachine(object):
 
         box = gtk.VBox(False, 6)
         box.pack_start(hbox, False, False, 0)
-        
+
         self.notebook = NotebookWithCloseButtonOnTabs()
         self.notebook.set_scrollable(True)
-        self.notebook.set_property('homogeneous', True)    
+        self.notebook.set_property('homogeneous', True)
         box.pack_start(self.notebook, True, True, 0)
 
         w.add(box)
@@ -199,10 +310,10 @@ class MainMachine(object):
     def new_tab(self, widget, component_name, component_class):
         child = MMMainFrame(component_class)
         child.show_all()
-        
+
         tab_label = gtk.Label(component_name)
         tab_label.show()
-        
+
         nbpages = self.notebook.get_n_pages()
         self.notebook.append_page(child, tab_label)
         self.notebook.set_current_page(nbpages)
