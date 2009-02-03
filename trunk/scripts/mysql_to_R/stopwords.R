c0c9f6b1a3e1e4012f588f5e66536eb128820b69
matteo.zandi <matteo.zandi@600039af-b535-0410-96a4-311d76c0c601>
2009-02-03 20:43:53 +0000

    * added scripts to easily extract data in R

---
+++ trunk/scripts/mysql_to_R/stopwords.R
@@ -0,0 +1,33 @@
+library(RMySQL)
+mycon <- dbConnect(MySQL(), user='testuser', dbname='martina',
+                   host='localhost', password='test')
+
+find_id <- function(name) {
+    query <- dbSendQuery(mycon, paste("select id
+                                       from intwords
+                                       where name='", name,"' and language_id=2600;", sep=''))
+
+    data <- fetch(query, n = -1)
+    data
+}
+
+stopwords = c('ad', 'a', 'e', 'i', 'o', 'con', 'da', 'di', 'in', 'su', 'per', 'tra', 'è', 'sono', 'lo', 'la', 'li', 'le', 'gli', 'ne', 'il', 'un', 'uno', 'una', 'ed', 'se', 'perché', 'come', 'che', 'chi', 'cui', 'non', 'quale', 'quanto', 'quanti', 'quanta', 'quante', 'quello', 'quelli', 'quella', 'quelle', 'questo', 'questi', 'questa', 'queste', 'si', 'no', 'sono')
+
+stopwords_ids = data.frame(NA)
+count = 1
+for (i in 1:length(stopwords)) {
+    id = find_id(stopwords[i])
+    
+    if (dim(id)[1]!=0) {
+        stopwords_ids[count,1] = stopwords[i]
+        stopwords_ids[count,2] = id[1,1]
+        count = count + 1
+    }
+}
+
+colnames(stopwords_ids) = c('stopword', 'id')
+
+stopwords_ids_list = ''
+for (i in 1:dim(stopwords_ids)[1]) {
+    stopwords_ids_list = paste(stopwords_ids_list, stopwords_ids[i,2], sep=', ')
+}
