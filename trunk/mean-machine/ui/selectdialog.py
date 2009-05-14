42c81ca950c311ad36ad2cf017eb3d36fa1ba779
battlehorse <battlehorse@600039af-b535-0410-96a4-311d76c0c601>
2009-05-14 14:29:45 +0000

    Using the right string-replace for the license name this time...

--- trunk/mean-machine/ui/selectdialog.py
+++ trunk/mean-machine/ui/selectdialog.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-# Licensed under the GNU General Public License v22 License.
+# Licensed under the GNU General Public License v2.
 
 __copyright__ = 'BayesFor Association'
 __author__    = 'Matteo Zandi <matteo.zandi@bayesfor.eu>'
