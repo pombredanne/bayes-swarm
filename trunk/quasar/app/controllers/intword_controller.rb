a77bb727e16f2b66646d4271c4207ca4556e1454
battlehorse <battlehorse@600039af-b535-0410-96a4-311d76c0c601>
2009-06-08 23:33:49 +0000

    Javascript autocomplete. Google News Search

--- trunk/quasar/app/controllers/intword_controller.rb
+++ trunk/quasar/app/controllers/intword_controller.rb
@@ -9,4 +9,24 @@ class IntwordController < ApplicationController
   
   def create
   end
+  
+  def ac
+    matches = {
+      'query' => params[:query] ,
+      'suggestions' => [],
+      'data' => [],
+    }
+    
+    Intword.find(
+      :all, 
+      :conditions => [ 'name like ? and iso_639_1 = ?', "#{params[:query]}%" , "#{params[:lang] || "en"}"], 
+      :order => "visible DESC",
+      :joins => "LEFT JOIN globalize_languages on globalize_languages.id = language_id",
+      :include => [ :language ],
+      :limit => 20).each do |iw| 
+        matches['suggestions'] << iw.name
+        matches['data'] << iw.id
+      end
+    render :json => matches.to_json
+  end
 end
