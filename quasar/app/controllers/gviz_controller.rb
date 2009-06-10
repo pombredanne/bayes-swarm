eac4dac246f6f918cf79a078af970dfeabe7d3cc
battlehorse <battlehorse@600039af-b535-0410-96a4-311d76c0c601>
2009-06-10 21:55:02 +0000

    Added 2 visualizations: motion chart and media pie

--- quasar/app/controllers/gviz_controller.rb
+++ quasar/app/controllers/gviz_controller.rb
@@ -5,7 +5,6 @@ class GvizController < GvizBaseController
     gvizifier do |gviz|
       pages = params[:source] && params[:source] != '0' ? Source.find(params[:source]).pages.map { |p| p.id } : nil
       
-      puts "Param is #{params[:kind]}"
       kind = Kind.find_by_kind(params[:kind])
       if kind
         kind = kind.kind
@@ -24,8 +23,41 @@ class GvizController < GvizBaseController
       gviz.set_data(merge(series))
     end
   end
+  
+  def motion
+    sdb = Sdb.new
+    gvizifier do |gviz|
+      kind = Kind.find_by_kind(params[:kind])
+      if kind
+        kind = kind.kind
+      end
+
+      csv_filename(params[:id], params[:entity], params[:from_date], params[:to_date])
+      
+      gviz.add_col('string', :id => 'name', :label => 'Word')
+      gviz.add_col('date', :id => 'scantime' , :label => 'Date')
+      # Add the columns for each source iterating by sorted key (source name), to preserve column alignment.      
+      Source.find(:all).map { |source| source.name }.sort.each_with_index do |sourcename, i|
+        gviz.add_col('number', :id => "count#{i}", :label => "#{sourcename}")
+      end
+      # TODO: should not allow more than x intwords.
+      iws = Intword.find(params[:id].split('-'))
+      # Always pass nil as the pages to select. Even though Aggregate.motion understands the parameter, at
+      # this moment do not allow multiple selection of sources, so it's all or nothing.
+      gviz.set_data(Aggregate.motion(sdb, params[:from_date], params[:to_date], iws, kind, params[:entity], nil))
+    end
+  end
     
-  def pie
+  def wordpie
+    pie(:intword)
+  end
+  
+  def pagepie
+    pie(:page)
+  end
+  
+  protected
+  def pie(by)
     sdb = Sdb.new
     gvizifier do |gviz|
       pages = params[:source] && params[:source] != '0' ? Source.find(params[:source]).pages.map { |p| p.id } : nil      
@@ -39,8 +71,9 @@ class GvizController < GvizBaseController
       gviz.add_col('string', :id => 'name', :label => 'name')
       gviz.add_col('number', :id => params[:entity], :label => params[:entity])
       iws = Intword.find(params[:id].split('-'))
-      gviz.set_data(Aggregate.pie(sdb, params[:from_date], params[:to_date], iws, kind, params[:entity], pages))
+      gviz.set_data(Aggregate.pie(sdb, by, params[:from_date], params[:to_date], iws, kind, params[:entity], pages))
     end
   end
+  
   
 end
--- quasar/app/models/aggregate.rb
+++ quasar/app/models/aggregate.rb
@@ -3,13 +3,13 @@ class Aggregate
   @@entities = [ 'count', 'bodycount', 'titlecount' , 'keywordcount' , 'anchorcount', 'headingcount' ]
   @@lowest_date = Date.civil(2007, 1, 1)
   
-  # TODO: should limit the number of intwords
-  def self.pie(sdb, from_date, to_date, intwords, kind=nil, entity='count', pages=nil)
+  # TODO: should limit the number of intwords  
+  def self.motion(sdb, from_date, to_date, intwords, kind=nil, entity='count', pages=nil)
     validate_entity(entity)
     low_date, high_date = validate_dates(from_date, to_date)
-
-    intword_name_map = {}
-    intwords.each { |iw| intword_name_map[iw.id] = iw.name }
+    
+    page_name_map = {}
+    (pages ? Page.find(pages) : Page.find(:all)).each { |page| page_name_map[page.id] = page.source.name }        
 
     domains = ((low_date.year)..(high_date.year)).map { |y| "Words#{y}"}
     conditions = { 
@@ -18,19 +18,67 @@ class Aggregate
     conditions[:page_id] = pages if pages      
     conditions[:page_kind] = kind if kind
     points = {}
-    sdb.select(domains, [ "id", "#{entity}"], conditions) do |item|
-      p = points[item['id'].first]
+    sdb.select(domains, [ "name", "id", "page_id", "#{entity}", "scantime"], conditions) do |item|
+      key = "#{item['id'].first}_#{item['scantime'].first}"
+      p = points[key]
+      unless p
+        p = MotionPoint.new
+        p.id = item['id'].first.to_i
+        p.name = item["name"].first
+        p.scantime = Date.strptime(item["scantime"].first, '%Y-%m-%d')
+        page_name_map.values.each do |source_name|
+          p.page_counts[source_name] = 0
+        end
+        points[key] = p
+      end
+      page_id = item['page_id'].first.to_i
+      source_name = page_name_map[page_id]
+      p.page_counts[source_name] += item[entity].first.to_i
+    end
+    return points.values.map do |p|
+      row = [ p.name, p.scantime ]
+      # Add the counts for each source iterating by sorted key (source name), to preserve column alignment.
+      p.page_counts.keys.sort.each { |key| row << p.page_counts[key] }
+      row
+    end
+  end
+  
+  # TODO: should limit the number of intwords
+  def self.pie(sdb, by, from_date, to_date, intwords, kind=nil, entity='count', pages=nil)
+    validate_entity(entity)
+    low_date, high_date = validate_dates(from_date, to_date)
+
+    key_name_map = {}
+    key = nil
+    if by == :intword
+      key = 'id'
+      intwords.each { |iw| key_name_map[iw.id] = iw.name }
+    elsif by == :page
+      key = 'page_id'
+      (pages ? Page.find(pages) : Page.find(:all)).each { |page| key_name_map[page.id] = page.source.name }        
+    end
+
+    domains = ((low_date.year)..(high_date.year)).map { |y| "Words#{y}"}
+    conditions = { 
+      :scantime => low_date.strftime('%Y-%m-%d')..high_date.strftime('%Y-%m-%d'), 
+      :id => ([] << intwords).flatten.map { |iw| iw.id } }
+    conditions[:page_id] = pages if pages      
+    conditions[:page_kind] = kind if kind
+    points = {}
+    sdb.select(domains, [ key, "#{entity}"], conditions) do |item|
+      name = key_name_map[item[key].first.to_i]
+      p = points[name]
       unless p
         p = AggregatePoint.new
-        p.intword_id = item['id'].first
-        p.count = 0
-        points[item['id'].first] = p
+        p.name = name
+        points[name] = p
+        p.count = 0        
       end
       p.count += item[entity].first.to_i
     end
-    return points.values.map { |p| [ intword_name_map[p.intword_id.to_i], p.count]}
-  end  
-  
+    return points.values.map { |p| [ p.name, p.count]}
+  end
+    
   def self.validate_entity(entity)
     unless @@entities.include?(entity)
       raise ArgumentError.new("Invalid entity #{entity}") 
@@ -65,5 +113,13 @@ class Aggregate
 end
 
 class AggregatePoint
-  attr_accessor :intword_id, :count
+  attr_accessor :name, :count
+end
+
+class MotionPoint
+  attr_accessor :name, :id, :scantime, :page_counts
+  
+  def initialize
+    @page_counts = {}
+  end
 end
