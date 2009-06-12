318b0b7b625edd2daf9d1e9b987483f51226bb0c
battlehorse <battlehorse@600039af-b535-0410-96a4-311d76c0c601>
2009-06-12 00:59:02 +0000

    Added validation for no or too many intwords

--- quasar/app/controllers/gviz_controller.rb
+++ quasar/app/controllers/gviz_controller.rb
@@ -14,8 +14,14 @@ class GvizController < GvizBaseController
       gviz.add_col('date', :id => 'scantime' , :label => 'Date')
       series = []
     
-      iws = Intword.find(params[:id].split('-'))
-      # TODO: should not allow more than x intwords.
+      unless params[:id].blank?
+        iws = Intword.find(params[:id].split('-'))
+        if iws.size > 10
+          raise ArgumentError.new('Too many keywords. You can use 10 at most')
+        end
+      else
+        raise ArgumentError.new('You must specify at least one keyword')
+      end
       iws.each do |iw|
         series << iw.time_series(sdb,  params[:from_date], params[:to_date], kind, params[:entity], pages)
         gviz.add_col('number', :id => iw.id, :label => iw.name)
@@ -40,8 +46,15 @@ class GvizController < GvizBaseController
       Source.find(:all).map { |source| source.name }.sort.each_with_index do |sourcename, i|
         gviz.add_col('number', :id => "count#{i}", :label => "#{sourcename}")
       end
-      # TODO: should not allow more than x intwords.
-      iws = Intword.find(params[:id].split('-'))
+      
+      unless params[:id].blank?
+        iws = Intword.find(params[:id].split('-'))
+        if iws.size > 10
+          raise ArgumentError.new('Too many keywords. You can use 10 at most')
+        end
+      else
+        raise ArgumentError.new('You must specify at least one keyword')
+      end      
       # Always pass nil as the pages to select. Even though Aggregate.motion understands the parameter, at
       # this moment do not allow multiple selection of sources, so it's all or nothing.
       gviz.set_data(Aggregate.motion(sdb, params[:from_date], params[:to_date], iws, kind, params[:entity], nil))
@@ -70,7 +83,15 @@ class GvizController < GvizBaseController
       csv_filename(params[:id], pages ? pages.join('-') : params[:entity], params[:from_date], params[:to_date])
       gviz.add_col('string', :id => 'name', :label => 'name')
       gviz.add_col('number', :id => params[:entity], :label => params[:entity])
-      iws = Intword.find(params[:id].split('-'))
+      
+      unless params[:id].blank?
+        iws = Intword.find(params[:id].split('-'))
+        if iws.size > 10
+          raise ArgumentError.new('Too many keywords. You can use 10 at most')
+        end
+      else
+        raise ArgumentError.new('You must specify at least one keyword')
+      end 
       gviz.set_data(Aggregate.pie(sdb, by, params[:from_date], params[:to_date], iws, kind, params[:entity], pages))
     end
   end
